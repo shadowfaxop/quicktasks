c8cf273650fc3916ac0642599dabc9581d61681e
Omkar Nikam <shadowfaxop@gmail.com>
2025-11-24 13:16:23 +0530

    Initial Commit

---
+++ TodoWidgetStarter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoWidgetStarter
+//
+//  Created by Omkar Nikam on 24/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoWidgetStarter/TodoWidgetStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoWidgetStarterApp.swift
+//  TodoWidgetStarter
+//
+//  Created by Omkar Nikam on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoWidgetStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
